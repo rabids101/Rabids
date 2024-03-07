f7730fa3d7fabd36b19e873ed24853a1f5d0ab74
kandelrabin55@gmail.com <kandelrabin55@gmail.com>
2024-03-07 13:38:38 +0545

    update script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ function sendMessage() {
   const userMessage = userMessageInput.value.trim();
     addUserMessage(userMessage);
     // Replace the bot's response with an actual response from your chatbot service or logic
-   if(userMessage=="15ysgca"){
+   if(userMessage=="Abc125El"){
     
   
     addBotMessage("eliteids.discord@mail.ru");
