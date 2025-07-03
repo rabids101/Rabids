c5b05634c6397532b00fbcebbfecbd9bc559a4db
vmelite3@gmail.com <vmelite3@gmail.com>
2025-07-03 08:55:04 +0545

    add

--- script2.js
+++ script2.js
@@ -8,7 +8,7 @@ function sendMessage() {
   const userMessage = userMessageInput.value.trim();
     addUserMessage(userMessage);
     // Replace the bot's response with an actual response from your chatbot service or logic
-       if(userMessage=="letsgo1"){
+       if(userMessage=="rabidszatw"){
 
   
     addBotMessage("idsrabdiscord@mail.ru");
