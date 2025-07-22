67f9a19e3899944246eb1a9346f5ca50301e58ce
vmelite3@gmail.com <vmelite3@gmail.com>
2025-07-22 07:27:46 +0545

    update script2.js

--- script2.js
+++ script2.js
@@ -12,7 +12,7 @@ function sendMessage() {
 
   
     addBotMessage("discordrabids@mail.ru");
-         addBotMessage("DontaddyournummZmhg17$(http://rabids.netlify.app)");
+         addBotMessage("DontaddyournummZmhg1@&$(http://rabids.netlify.app)");
 
     userMessageInput.value = "";
     chatBox.scrollTop = chatBox.scrollHeight;
