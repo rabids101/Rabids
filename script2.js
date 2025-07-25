f23528917ddbb4260d0a11bd5018299644d0d37d
vmelite3@gmail.com <vmelite3@gmail.com>
2025-07-25 07:10:37 +0545

    update script2.js

--- script2.js
+++ script2.js
@@ -8,11 +8,11 @@ function sendMessage() {
   const userMessage = userMessageInput.value.trim();
     addUserMessage(userMessage);
     // Replace the bot's response with an actual response from your chatbot service or logic
-       if(userMessage=="rabids@065"){
+       if(userMessage=="rabids&85"){
 
   
     addBotMessage("discordrabids@mail.ru");
-         addBotMessage("MFDontaddyournummZmhg8@&$(http://rabids.netlify.app)");
+         addBotMessage("MFDontaddyournummZmhg88@&$(http://rabids.netlify.app)");
 
     userMessageInput.value = "";
     chatBox.scrollTop = chatBox.scrollHeight;
